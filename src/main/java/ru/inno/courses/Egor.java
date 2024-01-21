cf2a5eb68ec9de38948cf04d7efaa4c04f286fc3
Egor Heybye <eshestakov@alfabank.ru>
2024-01-22 06:16:02 +0700

    first commit

--- src/main/java/ru/inno/courses/Egor.java
+++ src/main/java/ru/inno/courses/Egor.java
@@ -1,2 +1,7 @@
-package ru.inno.courses;public class Egor {
+package ru.inno.courses;
+
+public class Egor {
+    public static void main(String[] args) {
+        System.out.println("hello, world");
+    }
 }
