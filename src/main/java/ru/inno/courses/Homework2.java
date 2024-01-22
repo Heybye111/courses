b81b83c0e93fd97cd6d28375d3e4624e5b4276d2
Egor Heybye <eshestakov@alfabank.ru>
2024-01-22 07:59:33 +0700

    home work

---
+++ src/main/java/ru/inno/courses/Homework2.java
@@ -0,0 +1,26 @@
+package ru.inno.courses;
+
+public class Homework2 {
+
+    public static void main(String[] args) {
+
+        String cardNumber = "1234 5678 9012 3456";
+
+        String cardNumber2 = "1234567890123456";
+
+        String hideNumbers = "**** **** **** ";
+
+        String lastNumbers = cardNumber.substring(cardNumber.lastIndexOf(" ")+1);
+
+        String result = hideNumbers + lastNumbers;
+
+        System.out.println(result);
+
+        String sub = cardNumber.substring(cardNumber2.length() -1 );
+
+        System.out.println(hideNumbers + sub);
+
+
+    }
+
+}
