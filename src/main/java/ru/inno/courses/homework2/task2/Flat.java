6223a46b238429c74d9eaad0619578b78318c6a9
Egor Heybye <eshestakov@alfabank.ru>
2024-02-05 01:15:24 +0700

    update

--- src/main/java/ru/inno/courses/homework2/task2/Flat.java
+++ src/main/java/ru/inno/courses/homework2/task2/Flat.java
@@ -1,21 +1,21 @@
 package ru.inno.courses.homework2.task2;
 
 public class Flat {
-    String lobby;
-    String bathroom;
-    String kitchen;
-    String livingRoom;
-    String bedRoom;
+
     String price;
     double square;
     int floor;
     int roomCount;
     int flatNumber;
 
+
  public Flat(String lobby, String bathroom, String kitchen, String livingRoom, String bedRoom, String price, double square, int floor, int roomCount, int flatNumber) {
 
-     this.lobby = lobby;
-     this.bathroom = bathroom;
-     this.kitchen = kitchen;
+     this.price = price;
+     this.square = square;
+     this.floor = floor;
+     this.roomCount = roomCount;
+     this.flatNumber = flatNumber;
+
  }
 }
